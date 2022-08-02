2bcf4954feda4c270f503141bcf5ee505e1c91b3
shima98 <pytorch.python19@gmail.com>
2022-08-02 20:48:42 +0430

    add tuple

---
+++ tuple.py
@@ -0,0 +1,9 @@
+animals = ("lion", "tiger", "bear")
+print(animals[2])
+print(len(animals))
+for animal in animals :
+    print(animal)
+
+print(animals.index('tiger'))
+#animals[1] = 'elephant'
+#print(animals)
